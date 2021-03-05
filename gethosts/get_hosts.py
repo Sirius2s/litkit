20af42f3f14bd0a894ae8edb3c92e4f0086a69b0
Sirius2s <Sirius2s@gmail.com>
2021-03-05 14:25:04 +0800

    gethosts debug

--- gethosts/get_hosts.py
+++ gethosts/get_hosts.py
@@ -16,18 +16,18 @@ def get_ip(host):
         print("Unable to get IP of Hostname")
 
 def main():
-    f = open('gethosts/hosts.txt', 'w')
+    f = open('./gethosts/hosts.txt', 'w')
     f.write("# GitHub Start\n")
     f.close()
 
-    with open('gethosts/domain.txt', "r") as ins:
+    with open('./gethosts/domain.txt', "r") as ins:
         for host in ins:
             ip=get_ip(host.strip())
-            with open('gethosts/hosts.txt', 'a') as result:
+            with open('./gethosts/hosts.txt', 'a') as result:
                 result.write(ip.strip('\n') + " " + host)
                 print(ip.strip('\n') + " " + host)  # debug
 
-    f = open('gethosts/hosts.txt','a')
+    f = open('./gethosts/hosts.txt','a')
     f.write("\n# GitHub End\n")
     f.close()
 
