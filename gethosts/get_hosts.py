f8119cfe942792f13f3c4a9e3af5e0761e5ea17b
Sirius2s <Sirius2s@gmail.com>
2021-03-05 15:35:33 +0800

    gethost on

--- gethosts/get_hosts.py
+++ gethosts/get_hosts.py
@@ -1,9 +1,5 @@
 import socket
-import os
-import sys
 
-# reload(sys)
-# sys.setdefaultencoding("utf-8")
 
 def get_ip(host):
     """
@@ -15,21 +11,22 @@ def get_ip(host):
     except:
         print("Unable to get IP of Hostname")
 
+
 def main():
     f = open('./gethosts/hosts.txt', 'w')
-    f.write("# GitHub Start\n")
     f.close()
 
-    with open('./gethosts/domain.txt', "r") as ins:
-        for host in ins:
-            ip=get_ip(host.strip())
+    with open('./gethosts/domain.txt', "r") as lins:
+        for host in lins:
+            if '#' in host or host == '' or host == '\n':
+                with open('./gethosts/hosts.txt', 'a') as result:
+                    result.write(host)
+                    continue
+
+            ip = get_ip(host.strip())
             with open('./gethosts/hosts.txt', 'a') as result:
-                result.write(ip.strip('\n') + " " + host)
-                print(ip.strip('\n') + " " + host)  # debug
+                result.write(ip.strip('\n') + "\t" + host)
 
-    f = open('./gethosts/hosts.txt','a')
-    f.write("\n# GitHub End\n")
-    f.close()
 
 if __name__ == "__main__":
     main()
